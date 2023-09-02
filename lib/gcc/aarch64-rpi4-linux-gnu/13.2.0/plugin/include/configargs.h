8097799f3c8399b316dec4b9c0838e09e09a89f0
AhmedAlyEl-Ghannam <ahmedaly.g112@gmail.com>
2023-09-02 09:04:06 +0300

    Custom toolchain is alive and kicking

---
+++ lib/gcc/aarch64-rpi4-linux-gnu/13.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/a_destructor/Desktop/crosstool-ng/.build/aarch64-rpi4-linux-gnu/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=aarch64-rpi4-linux-gnu --prefix=/home/a_destructor/x-tools/aarch64-rpi4-linux-gnu --exec_prefix=/home/a_destructor/x-tools/aarch64-rpi4-linux-gnu --with-sysroot=/home/a_destructor/x-tools/aarch64-rpi4-linux-gnu/aarch64-rpi4-linux-gnu/sysroot --enable-languages=c,c++ --with-cpu=cortex-a72 --with-pkgversion='crosstool-NG 1.26.0_rc1.2_cf25b4e' --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --disable-libmpx --with-gmp=/home/a_destructor/Desktop/crosstool-ng/.build/aarch64-rpi4-linux-gnu/buildtools --with-mpfr=/home/a_destructor/Desktop/crosstool-ng/.build/aarch64-rpi4-linux-gnu/buildtools --with-mpc=/home/a_destructor/Desktop/crosstool-ng/.build/aarch64-rpi4-linux-gnu/buildtools --with-isl=/home/a_destructor/Desktop/crosstool-ng/.build/aarch64-rpi4-linux-gnu/buildtools --enable-lto --enable-threads=posix --enable-target-optspace --enable-plugin --enable-gold --disable-nls --disable-multilib --with-local-prefix=/home/a_destructor/x-tools/aarch64-rpi4-linux-gnu/aarch64-rpi4-linux-gnu/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "cortex-a72" } };
